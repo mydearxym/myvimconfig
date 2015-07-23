79393388c86b760d69f560adcfa075c4b08b0d25
mydearxym <mydearxym@qq.com>
2015-07-23 11:09:55 +0800

    backup vim config

---
+++ backup.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+
+echo "backuping vim config ..."
+cp -rf ~/.vim/*  ./vim/
+cp ~/.vimrc  vimrc
+cp ~/.vimrc.local  vimrc.local
+cp ~/.vimrc.bundles  vimrc.bundles
+cp ~/.vimrc.bundles.local  vimrc.bundles.local
+echo "copy done !"
+echo "add to github "
+git add .
+git commit -am "backup vim config "
+git push origin master
